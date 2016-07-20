7ad09621aa6a127f2581ebd961b29358fa169ca4
mdos-san <mdos-san@student.42.fr>
2016-07-20 14:28:22 +0200

    [dev]: Clearing screen and render a welcome message

--- srcs/main.c
+++ srcs/main.c
@@ -5,6 +5,8 @@ int		main(int ac, char **av, char **environ)
 	t_mnsh	mnsh;
 
 	mnsh = mnsh_init(ac, av, environ);
+	ft_putstr("\e[1;1H\e[2J");
+	ft_putendl("Welcome to minishell :D !\n");
 	mnsh_start(&mnsh);
 	mnsh_free(&mnsh);
 	return (0);
