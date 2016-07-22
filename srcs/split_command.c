0a563cf78fdeb4322401066bac9b95c645ff2ca0
mdos-san <mdos-san@student.42.fr>
2016-07-22 21:21:56 +0200

    [dev]: command work with space before haha

--- srcs/split_command.c
+++ srcs/split_command.c
@@ -2,15 +2,6 @@
 
 void	split_command(t_mnsh *mnsh)
 {
-	int	index;
-
-	index = 0;
 	mnsh->parameters = ft_strsplit(mnsh->command, ' ');
-	while (index + 1 != '\0')
-	{
-		(mnsh->command[index] == ' ') ? (mnsh->command[index] = '\0') : 0;
-		if (mnsh->command[index] == '\0')
-			break ;
-		++index;
-	}
+	mnsh->command = ft_strdup(mnsh->parameters[0]);
 }
