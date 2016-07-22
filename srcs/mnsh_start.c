3ea3868f66dfb578e1f3ad0301be1c47833f2306
mdos-san <mdos-san@student.42.fr>
2016-07-22 21:17:43 +0200

    [dev]: command not found error

--- srcs/mnsh_start.c
+++ srcs/mnsh_start.c
@@ -2,13 +2,20 @@
 
 void	mnsh_start(t_mnsh *mnsh)
 {
+	int	ret;
+
+	ret = 0;
 	while (mnsh->command == NULL || ft_strcmp("exit", mnsh->command) != 0)
 	{
 		ft_strdel(&mnsh->command);
 		mnsh->command = command_get();
 		split_command(mnsh);
 		(built_in_check(mnsh) == 0)
-		? bin_check(mnsh)
-		: 0;
+		? (ret = bin_check(mnsh)) : 0;
+		if (ret == 0)
+		{
+			ft_putstr(mnsh->command);
+			ft_putendl(": command not found");
+		}
 	}
 }
