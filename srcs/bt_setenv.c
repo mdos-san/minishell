b1843cd62fadfdc9d6fac535cc0b984c317d8c09
Mickael DOS SANTOS <mdos-san@e2r8p4.42.fr>
2016-08-05 12:14:52 +0200

    [dev]: fixing setenv

--- srcs/bt_setenv.c
+++ srcs/bt_setenv.c
@@ -6,7 +6,7 @@
 /*   By: mdos-san <mdos-san@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/05 10:01:29 by mdos-san          #+#    #+#             */
-/*   Updated: 2016/08/05 10:01:32 by mdos-san         ###   ########.fr       */
+/*   Updated: 2016/08/05 12:07:43 by mdos-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 void	bt_setenv(t_mnsh *mnsh)
 {
-	str_array_add(&mnsh->env, mnsh->parameters[1]);
+	char	*left;
+	char	*right;
+
+	if (str_array_count(mnsh->parameters) == 3)
+	{
+		left = ft_strjoin(mnsh->parameters[1], "=");
+		right = ft_strjoin(left, mnsh->parameters[2]);
+		str_array_add(&mnsh->env, right);
+		ft_strdel(&left);
+		ft_strdel(&right);
+	}
 }
